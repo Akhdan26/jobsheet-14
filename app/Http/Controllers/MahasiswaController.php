09739a35634a106986440756194e87a7b30b9211
Muhammad Hifzhan Silmi <56281943+Akhdan26@users.noreply.github.com>
2020-05-14 20:08:27 +0700

    jobsheet-14

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Mahasiswa;
+
+class MahasiswaController extends Controller
+{
+    //fungsi index digunakan untuk menampilkan semua data mahasiswa
+    public function index(){
+        $data = Mahasiswa::all();
+
+        //cek data tidak kosong
+        if(count($data) > 0){
+            $res['message'] = 'Success!';
+            $res['values'] = $data;
+            return response($res);
+        }
+        //jika data kosong
+        else{
+            $res['message'] = 'Kosong!';
+            return response($res);
+        }
+    }
+    
+    public function getID($id){
+        $data = Mahasiswa::where('id',$id)->get();
+
+        //cek data tidak kosong
+        if(count($data) > 0){
+            $res['message'] = 'Success!';
+            $res['values'] = $data;
+            return response($res);
+        }
+        //jika data kosong
+        else{
+            $res['message'] = 'Kosong!';
+            return response($res);
+        }
+    }
+
+    //fungsi tambah data
+    public function create(Request $request){
+        $mhs = new Mahasiswa();
+        $mhs->nama = $request->nama;
+        $mhs->nim = $request->nim;
+        $mhs->email = $request->email;
+        $mhs->jurusan = $request->jurusan;
+
+        //jika data berhasil tersimpan
+        if($mhs->save()){
+            $res['message'] = 'Data Berhasil ditambah!';
+            $res['values'] = "$mhs";
+            return response($res);
+        }
+    }
+    
+    public function update(Request $request,$id){
+        $nama = $request->nama;
+        $nim = $request->nim;
+        $email = $request->email;
+        $jurusan = $request->jurusan;
+        
+        $mhs = Mahasiswa::find($id);
+        $mhs->nama = $nama;
+        $mhs->nim = $nim;
+        $mhs->email = $email;
+        $mhs->jurusan = $jurusan;
+
+        //jika data berhasil tersimpan
+        if($mhs->save()){
+            $res['message'] = 'Data Berhasil diubah!';
+            $res['values'] = "$mhs";
+            return response($res);
+        }else{
+            $res['message'] = 'Data gagal diubah!';
+            return response($res);
+        }
+    }
+
+    public function delete($id){
+        $mhs = Mahasiswa::where('id',$id);
+
+        if($mhs->delete()){
+            $res['message'] = "Data berhasil dihapus!";
+            return response($res);
+        }else{
+            $res['message'] = "Data gagal dihapus!";
+            return response($res);
+        }
+    }
+}
